2f0a772cdb86ab94b4b2a743a19351a5323152fd
Saeed Sabra <saeedsabra565@gmail.com>
2023-06-20 13:13:13 +0300

    logout user

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.send("GET requests are disable!");
+//   } else {
+//     next();
+//   }
+// });
+
+// app.use((req, res, next) => {
+//   res.status(503).send("Server Under work!");
+// });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -29,14 +41,25 @@ app.listen(port, () => {
 
 // myFunction();
 
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
-const myFunction = async () => {
-  const token = jwt.sign({ _id: "saeed123" }, "thisismynewcourse");
-  console.log(token);
+// const myFunction = async () => {
+//   const token = jwt.sign({ _id: "saeed123" }, "thisismynewcourse");
+//   console.log(token);
 
-  const data = jwt.verify(token, "thisismynewcourse");
-  console.log(data);
+//   const data = jwt.verify(token, "thisismynewcourse");
+//   console.log(data);
+// };
+
+// myFunction();
+
+const pet = {
+  name: "hal",
 };
 
-myFunction();
+pet.toJSON = function () {
+  // console.log(this);
+  return {};
+};
+
+console.log(JSON.stringify(pet));
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../models/user");
+const auth = require("../middleware/auth");
 const router = new express.Router();
 
 router.post("/users", async (req, res) => {
@@ -33,21 +34,31 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.get("/users", async (req, res) => {
+router.post("/users/logout", auth, async (req, res) => {
   try {
-    const users = await User.find({});
-    res.send(users);
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+
+    res.send();
   } catch (e) {
     res.status(500).send();
   }
+});
 
-  // User.find({})
-  //   .then((users) => {
-  //     res.send(users);
-  //   })
-  //   .catch((error) => {
-  //     res.status(400).send(error);
-  //   });
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get("/users/:id", async (req, res) => {
